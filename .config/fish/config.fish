efb5af88771ac2b6621cb206900b3f8235d3966a
Luke D Russell <lukedrussell+git@outlook.com>
2020-04-23 19:01:05 +1000

    fish and simple tmux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,3 +9,6 @@ alias ll="exa -l"
 fish_vi_key_bindings
 status --is-interactive; and pyenv init - | source
 status --is-interactive; and pyenv virtualenv-init - | source
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
