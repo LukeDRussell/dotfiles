c8657af0cd1e101a552d13c2438705539cf0f39c
Luke D Russell <LukeDRussell+git@outlook.com>
2020-08-02 09:54:06 +1000

    better tmux status bar

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
+# First time setup.sh
 
-
+# Install Python Poetry
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
-poetry completions fish > ~/.config/fish/completions/poetry.fish
 
+# Install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Install Ansible
+sudo yum install ansible
