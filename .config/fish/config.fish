981a279dfd397866424dc3122469c6d746fa4444
Luke D Russell <LukeDRussell+git@outlook.com>
2020-08-31 20:21:18 +1000

    moving to desktop

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,8 @@
 # Start Tmux
 tmux attach-session || tmux new-session
+register-python-argcomplete --shell fish pipx | .
+# Register pipx completions
+
 
 # Alias
 
@@ -10,3 +13,6 @@ alias ll="exa -l"
 
 # Settings
 fish_vi_key_bindings
+
+# Created by `userpath` on 2020-08-20 21:12:17
+set PATH $PATH /home/luke/.local/bin
