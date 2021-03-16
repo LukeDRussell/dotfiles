c24b5683f49469eff41d02fa6c4e50cd2d494e5b
Luke Russell <Luke.Russell@psba.qld.gov.au>
2021-03-17 08:36:28 +1000

    tmux-256color terminal profile added

--- Darwin.sh
+++ Darwin.sh
@@ -19,3 +19,9 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 # Change the default shell to fish
 # build the latest version of python using pyenv
 # set latest version of python to global with pyenv
+
+# ITerm2 profiles to use Tmux (if session exists attach, otherwise create and attach)
+# /usr/local/bin/tmux new -A
+
+# Some MacOS built apps use the ancient built-in ncurses, which is missing tmux-256 terminal profile.
+# /usr/bin/tic -x tmux-256color
