60816d77c26bc9b588ee2e290c59ce6d6d2d53d0
Luke D Russell <lukedrussell+git@outlook.com>
2021-04-03 09:01:41 +1000

    asdfsadf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,10 +6,13 @@ alias ls=exa
 alias ll="exa -l"
 alias tree="exa --tree"
 
-# Settings
-fish_vi_key_bindings
-
-# pyenv init
+# Use pyenv shim if it exists
 if command -v pyenv 1>/dev/null 2>&1
   pyenv init - | source
 end
+
+# Disable welcome
+set -U fish_greeting
+
+# Use vim bindings
+fish_vi_key_bindings
