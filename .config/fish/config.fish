506f32f21ac8a91ad4cc1c6adc32971349feedfc
Luke D Russell <lukedrussell+git@outlook.com>
2021-04-03 09:08:18 +1000

    Check if neovim and exa are actually installed before using them.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,19 @@
-# Alias
-
-alias vi=nvim
-alias vim=nvim
-alias ls=exa
-alias ll="exa -l"
-alias tree="exa --tree"
-
 # Use pyenv shim if it exists
 if command -v pyenv 1>/dev/null 2>&1
   pyenv init - | source
+end
+
+# Use neovim if it exists
+if command -sq nvim 
+  alias vi=nvim
+  alias vim=nvim
+end
+
+# Use exa if it exists
+if command -sq exa
+  alias ls=exa
+  alias ll="exa -l"
+  alias tree="exa --tree"
 end
 
 # Disable welcome
@@ -16,3 +21,4 @@ set -U fish_greeting
 
 # Use vim bindings
 fish_vi_key_bindings
+
