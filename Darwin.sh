9e33664e805bffd7d1d3e809ea93c62b378a5002
Luke D Russell <lukedrussell+git@outlook.com>
2021-04-03 09:01:41 +1000

    asdfsadf

--- Darwin.sh
+++ Darwin.sh
@@ -2,16 +2,17 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # CLI Apps
-brew install pyenv pyenv-virtualenv exa tmux jq git ansible fish tldr neovim
+brew install fish pyenv pyenv-virtualenv exa tmux jq git ansible fish tldr neovim
 
 # GUI Apps
 brew install --cask visual-studio-code iterm2 drawio
 
 # Link Fish config
 mkdir ~/.config
-ln -s ~/Repos/dotfiles/.config/fish ~/.config/fish
-ln -s ~/Repos/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/Repos/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/OneDrive/Repos ~/Repos
+ln -sf ~/Repos/dotfiles/.config/fish ~/.config/fish
+ln -sf ~/Repos/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/Repos/dotfiles/.tmux.conf ~/.tmux.conf
 
 # Disable silly keyboard shortcuts
 defaults write -g ApplePressAndHoldEnabled -bool false
