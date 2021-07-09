eb4b983b97a544328fa0a936aa030c5fd563e976
Luke D Russell <LukeDRussell+git@outlook.com>
2021-07-09 23:17:39 +1000

    got rid of an annoying pyenv-virtualenv error

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,11 @@
 # fish_add_path requires fish 3.2.1
 
-# PyEnv
+# Pyenv
 if command -sq pyenv
-  status is-login; and pyenv init --path | source
+  status is-interactive; and pyenv init --path | source
   pyenv init - | source
-  pyenv virtualenv-init - | source
+  status is-interactive; and pyenv-virtualenv-init - | source
+  pyenv-virtualenv-init - | source
 end
 
 # pipx
@@ -23,6 +24,8 @@ if command -sq exa
   alias ls=exa
   alias ll="exa -l"
   alias tree="exa --tree"
+else
+  alias ll="ls -l"
 end
 
 # Disable welcome
@@ -33,6 +36,8 @@ fish_vi_key_bindings
 
 # Ansible
 if command -sq ansible
+  alias play="ansible-playbook"
   alias play_askpass="ansible-playbook -k"
   alias play_vaultpass="ansible-playbook -e @~/.vault.yml --vault-password-file ~/.vault-pw"
 end
+
