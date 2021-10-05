fc69895aee04554dac566c050d82a062221a1904
Luke D Russell <lukedrussell+git@outlook.com>
2021-10-05 23:04:44 +1000

    cool sidescrolling in nvim v2

--- init.lua
+++ init.lua
@@ -21,7 +21,7 @@ require('hardline').setup {
 
 -- My Settings
 vim.opt.scrolloff = 5
-vim.opt.sidescroll= 5
+vim.opt.sidescroll= 1
 vim.opt.number = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
