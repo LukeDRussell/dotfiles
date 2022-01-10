0c85de40c06fc63fdf40c59dbf75bac03fc0adfd
Luke D Russell <LukeDRussell+git@outlook.com>
2022-01-10 11:09:18 +1000

    minor fixes to status bar

--- init.lua
+++ init.lua
@@ -8,19 +8,41 @@ require "paq" {
 	"neovim/nvim-lspconfig";
 	"nvim-lua/plenary.nvim";
 	"nvim-telescope/telescope.nvim";
-	"kristijanhusak/orgmode.nvim";
+	"nvim-orgmode/orgmode.nvim";
 	"ojroques/nvim-hardline";
 	"junegunn/goyo.vim";
 	"junegunn/limelight.vim";
-	"lukas-reineke/indent-blankline.nvim"
+	"lukas-reineke/indent-blankline.nvim";
+	"chentau/marks.nvim";
+	"ishan9299/nvim-solarized-lua"
 }
 
+-- Language Servers
+-- Python
+require'lspconfig'.pylsp.setup{}
+
+
 -- hardline status and tab line
-require('hardline').setup {
+require'hardline'.setup {
 	theme = 'default'
 }
 
--- Org-mode vim
+require'marks'.setup {
+	mappings = {
+		toggle = "m,";
+		delete_line = "dm.";
+		next = "m<down>";
+		prev = "m<up>"
+	}
+}
+
+-- orgmode.nvim
+require'orgmode'.setup {
+	org_agenda_files = '~/Notes/org/*';
+	org_default_notes_file = '~/Notes/org/notes.org'
+}
+
+
 
 -- My Settings
 vim.opt.scrolloff = 5
@@ -29,7 +51,10 @@ vim.opt.number = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
+vim.o.bg = 'dark'
+vim.opt.undofile = true
 
+-- File type settings
 -- yaml
--- expandtab indentkeys-=0# indentkeys-=<:>
 
+vim.api.nvim_exec([[ autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab indentkeys-=0# indentkeys-=<:> ]], false)
