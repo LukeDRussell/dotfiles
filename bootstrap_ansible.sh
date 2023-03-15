4b2ea152a691af8f43cb6d55aa3ab09c982a0a98
Luke Russell <luke@Lukes-MBP.local>
2023-03-15 20:26:58 +1000

    lsd with nice dateformat and hides symlink targets:

--- bootstrap_ansible.sh
+++ bootstrap_ansible.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
-# This script uses ansible to install dotfiles.
-
 # Set OS & Distro variables
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	DISTRO="sed -n -e 's/^ID_LIKE=//p' /etc/os-release"
-	if [[ "$DISTRO" == "fedora"||"centos" ]]; then
+	if [[ "$DISTRO" == "fedora" ]]; then
+		CMD_PKG_INSTALL="dnf"
+	elif [[ "$DISTRO" == "centos"||"redhat" ]]; then
 		CMD_PKG_INSTALL="yum"
 	elif [[ "$DISTRO" == "debian"||"ubuntu" ]]; then
 		CMD_PKG_INSTALL="apt"
 	else
-		echo "Check '\$DISTRO' has a match condition: $DISTRO"
+		echo "This script wasn't written for your distro: $DISTRO"
 	fi
+
 	if [[ "id -u" != 0 ]]; then
 		SUDO="sudo"
 	else
@@ -34,4 +35,3 @@ if ! command -v ansible &> /dev/null ; then
 	$SUDO $CMD_PKG_INSTALL install ansible -y
 fi
 
-ansible-playbook main.yml -K
