6fd482d990b012d0ca56be5f4db2809a6a6626e2
Luke D Russell <luke@russelltech.au>
2024-10-30 16:23:39 +1000

    manually merging f-shells branch

--- install-ansible.sh
+++ install-ansible.sh
@@ -2,16 +2,26 @@
 
 # This script uses ansible to install dotfiles, so make sure it's installed.
 
+if command -v ansible &> /dev/null ; then
+	echo "Ansible is ready."
+	exit 0
+fi
+
+echo "Installing Ansible..."
+
 # Set OS & Distro variables
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	DISTRO="sed -n -e 's/^ID_LIKE=//p' /etc/os-release"
-	if [[ "$DISTRO" == "fedora"||"centos" ]]; then
+	if [[ "$DISTRO" == "fedora" ]]; then
+		CMD_PKG_INSTALL="dnf"
+	elif [[ "$DISTRO" == "centos"||"redhat" ]]; then
 		CMD_PKG_INSTALL="yum"
 	elif [[ "$DISTRO" == "debian"||"ubuntu" ]]; then
 		CMD_PKG_INSTALL="apt"
 	else
-		echo "Check '\$DISTRO' has a match condition: $DISTRO"
+		echo "This script wasn't written for your distro: $DISTRO"
 	fi
+
 	if [[ "id -u" != 0 ]]; then
 		SUDO="sudo"
 	else
@@ -26,14 +36,9 @@ elif [[ "$OSTYPE" == "darwin".* ]]; then
 		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	fi
 	SUDO = ""
+else
+	echo "Can't determine package installer. Check your OS is in the list"
+	exit 1
 fi
 
-# Makes sure Ansible is ready to go
-if ! command -v ansible &> /dev/null ; then
-	echo "Installing Ansible..."
-	$SUDO $CMD_PKG_INSTALL install ansible -y
-fi
-
-if command -v ansible &> /dev/null ; then
-	echo "Ansible is ready."
-fi
+$SUDO $CMD_PKG_INSTALL install ansible -y
